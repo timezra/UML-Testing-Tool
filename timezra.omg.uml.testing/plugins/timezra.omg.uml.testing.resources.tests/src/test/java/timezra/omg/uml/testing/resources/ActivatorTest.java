3865febf59f6c59801a4d92a17ff3ce79f9e543b
Tim Myer <timezra@gmail.com>
2014-04-17 10:14:24 -0700

    Initial commit with skeleton project structure.

---
+++ timezra.omg.uml.testing/plugins/timezra.omg.uml.testing.resources.tests/src/test/java/timezra/omg/uml/testing/resources/ActivatorTest.java
@@ -0,0 +1,13 @@
+package timezra.omg.uml.testing.resources;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class ActivatorTest {
+
+	@Test
+	public void thePluginShouldBeActivated() {
+		assertNotNull(Activator.getDefault());
+	}
+}
